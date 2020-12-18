005513eb8f57c5746d6288ac6b66fcd52fbad785
Jrjuniorjr <mjuniorjrjr@outlook.com>
2020-12-18 12:17:39 -0300

    Fazendo CitiesController herdar de ControllerBase, ganhando algumas funcionalidades.

--- CityInfo/CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CityInfo.API.Controllers
 {
-    public class CitiesController
+    public class CitiesController : ControllerBase
     {
     }
 }

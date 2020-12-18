b97960aca7bbd2b92b0ddfed08a6c79f5b654734
Jrjuniorjr <mjuniorjrjr@outlook.com>
2020-12-18 12:26:19 -0300

    Criando metodo GetCities para retornar dados.

--- CityInfo/CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -9,5 +9,14 @@ namespace CityInfo.API.Controllers
     [ApiController]
     public class CitiesController : ControllerBase
     {
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>()
+                {
+                    new { id = 1, Name = "New York City" },
+                    new { id = 2, Name = "Antwerp" }
+                });
+        }
     }
 }

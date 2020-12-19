74eb12a0da32708588c13ad868f7baba2bf22fdc
Jrjuniorjr <mjuniorjrjr@outlook.com>
2020-12-19 16:39:59 -0300

    Adicionando RouteAttribute em CitiesController e HttpGetAttribute no metodo GetCities.

--- CityInfo/CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -7,8 +7,10 @@ using System.Threading.Tasks;
 namespace CityInfo.API.Controllers
 {
     [ApiController]
+    [Route("api/[controller]")]
     public class CitiesController : ControllerBase
     {
+        [HttpGet]
         public JsonResult GetCities()
         {
             return new JsonResult(

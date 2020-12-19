1bdba4c626023d5f19958a789ee333619dc127b0
Jrjuniorjr <mjuniorjrjr@outlook.com>
2020-12-19 18:19:23 -0300

    Criando metodo GetPointsOfInterest para retornar um ponto de interesse de uma cidade.

--- CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -24,5 +24,27 @@ namespace CityInfo.API.Controllers
             return Ok(city.PointsOfInterest);
 
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetPointOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities
+                .FirstOrDefault(c => c.Id == cityId);
+
+            if(city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterest = city.PointsOfInterest
+                .FirstOrDefault(p => p.Id == id);
+
+            if(pointOfInterest == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pointOfInterest);
+        }
     }
 }

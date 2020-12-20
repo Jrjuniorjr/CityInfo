dcbeddb988f72922ab32d28e116669d5cb1af3c6
Jrjuniorjr <mjuniorjrjr@outlook.com>
2020-12-20 11:55:47 -0300

    Criando validações que são mais complexas, ou seja, que não é possível com data annotations. Nessa situação, é necessário checar a validação e adicionar ao ModelState o erro em questão. IMPORTANTE: Também é necessário checar a propriedade isValid do ModelState, pois nessa situação, não tem data annotations e o ApiControllerAttribute só faz isso quando existe data annotation e quando ele vai fazer a deserialização do body da requisição para o model, por isso, fica muito tarde para o ApiControllerAttribute fazer a checagem do ModelState. O Model Binding já aconteceu, e após isso, é que a gente adiciona o erro ao ModelState, por isso é necessário essa checagem.

--- CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -52,6 +52,17 @@ namespace CityInfo.API.Controllers
         public IActionResult CreatePointOfInterest(int cityId,
             [FromBody]PointOfInterestForCreationDto pointOfInterest)
         {
+            if(pointOfInterest.Description == pointOfInterest.Name)
+            {
+                ModelState.AddModelError(
+                    "Description",
+                    "The provided description should be different from the name.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
             var city = CitiesDataStore.Current.Cities
                 .FirstOrDefault(c => c.Id == cityId);
             if(city == null)

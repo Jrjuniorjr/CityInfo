19d96d165daa88e0f5d9b24b4eeaa9fa405b808d
Jrjuniorjr <mjuniorjrjr@outlook.com>
2020-12-23 21:23:48 -0300

    Adicionando em CityInfoContext para reforçar a criação do banco caso nao exista.

--- CityInfo.API/Contexts/CityInfoContext.cs
+++ CityInfo.API/Contexts/CityInfoContext.cs
@@ -12,6 +12,12 @@ namespace CityInfo.API.Contexts
         public DbSet<City> Cities { get; set; }
         public DbSet<PointOfInterest> PointsOfInterest { get; set; }
 
+        public CityInfoContext(DbContextOptions<CityInfoContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
         //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         //{
         //    optionsBuilder.UseSqlServer("connectionstring");
